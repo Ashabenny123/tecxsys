fb7c25f320f7d90a03839fed661cc29a374977e1
scribblepanda <akshaik143@gmail.com>
2022-08-30 01:41:03 +0530

    progress

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
-  AOS.init();
+var leftItem = document.getElementsByClassName("box-l"),
+  rightItem = document.getElementsByClassName("box-r");
+
+(function () {
+  var throttle = function (type, name, obj) {
+    var obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+
+  throttle("scroll", "optimizedScroll");
+})();
+
+window.addEventListener("optimizedScroll", function () {
+  Array.prototype.forEach.call(leftItem, function (el) {
+    // Do stuff here
+    el.style.transform = "rotate(-" + window.pageYOffset + "deg)";
+  });
+  Array.prototype.forEach.call(rightItem, function (el) {
+    // Do stuff here
+    el.style.transform = "rotate(" + window.pageYOffset + "deg)";
+  });
+
+  //rightItem.style.transform = "rotate(" + window.pageYOffset + "deg)";
+});
